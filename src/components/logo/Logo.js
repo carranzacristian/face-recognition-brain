e4080d766747a2165da187f38c95d8d5a09b2d4b
cristian carranza <hncristiansoriano@users.noreply.github.com>
2021-06-23 00:09:29 -0400

    removed needless code

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,12 +4,14 @@ import './Logo.css';
 import brain from './brainLogo2.png';
 
 const Logo = () => {
-	const nodeRef = React.useRef(null);
 	return (
 		<div className='ma4 mt4' style={{width: '150px'}}> 
-			<Tilt nodeRef={nodeRef} className="Tilt br2 shadow-2" options={{ max : 55 }} style={{position: 'relative', zIndex: '3', height: 150, width: 150, marginRight: 0 }} >
- 				<div ref={nodeRef} className="Tilt-inner pa3" style={{width: '100%', height:'100%'}}>
- 					<img  alt='logo' src={brain}/></div>
+			<Tilt className="Tilt br2 shadow-2"
+			options={{ max : 55 }}
+			style={{position: 'relative', zIndex: '3', height: 150, width: 150, marginRight: 0 }} >
+ 				<div className="Tilt-inner pa3" style={{width: '100%', height:'100%'}}>
+ 					<img  alt='logo' src={brain}/>
+				</div>
 			</Tilt>
 		</div>
 	);
